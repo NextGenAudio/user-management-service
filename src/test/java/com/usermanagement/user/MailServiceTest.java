74d1d1342dc53bf3bbb4937c11209652f51364b1
Dinushka-Aththanayake <161476349+Dinushka-Aththanayake@users.noreply.github.com>
2025-10-17 14:12:40 +0530

    added new tests for mail service

--- src/test/java/com/usermanagement/user/MailServiceTest.java
+++ src/test/java/com/usermanagement/user/MailServiceTest.java
@@ -1,39 +1,58 @@
 package com.usermanagement.user;
 
+import com.usermanagement.user.domain.service.MailService;
 import com.usermanagement.user.utill.Jwtutil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
 
 @SpringBootTest
 public class MailServiceTest {
-    private Jwtutil jwtUtil;
+    @Mock
+    private JavaMailSender mailSender;
+    private MailService mailService;
 
     @BeforeEach
     void setUp() {
-        jwtUtil = new Jwtutil();
+        MockitoAnnotations.openMocks(this);
+        mailService = new MailService(mailSender);
     }
 
     @Test
-    void generateAndValidateToken_Success() {
-        String username = "test@test.com";
-        String token = jwtUtil.generateToken(username);
+    void sendEmail_Success() {
+        String to = "test@test.com";
+        String subject = "Test Subject";
+        String body = "Test Body";
 
-        assertNotNull(token);
-        assertTrue(jwtUtil.validateToken(token, username));
-        assertEquals(username, jwtUtil.extractUsername(token));
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        mailService.sendEmail(to, subject, body);
+
+        verify(mailSender, times(1)).send(any(SimpleMailMessage.class));
     }
 
     @Test
-    void validateToken_WrongUsername() {
-        String username = "test@test.com";
-        String wrongUsername = "wrong@test.com";
-        String token = jwtUtil.generateToken(username);
+    void sendEmail_Failure() {
+        String to = "test@test.com";
+        String subject = "Test Subject";
+        String body = "Test Body";
 
-        assertFalse(jwtUtil.validateToken(token, wrongUsername));
+        doThrow(new RuntimeException("Mail server error"))
+                .when(mailSender).send(any(SimpleMailMessage.class));
+
+        assertThrows(RuntimeException.class, () ->
+                mailService.sendEmail(to, subject, body)
+        );
     }
 
 }
